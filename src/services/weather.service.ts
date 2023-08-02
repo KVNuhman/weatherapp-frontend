3ce5ec8367cf252810925f615057c579d69ed9ab
Nuhman KV <kvnuhman@gmail.com>
2023-08-02 15:47:01 +0530

    first commit

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -7,38 +7,17 @@ import { WeatherData } from 'src/models/weather.model';
   providedIn: 'root'
 })
 export class WeatherService {
-  // weatherApiBseUrl = 'https://openweather43.p.rapidapi.com/weather';
-  // XRapidAPIHostHeaderName = 'X-RapidAPI-Host';
-  // XRapidAPIHostHeaderValue = 'openweather43.p.rapidapi.com';
-  // XRapidAPIKeyHeaderName = 'X-RapidAPI-Key';
-  // XRapidAPIKeyHeaderValue = '27bbfa8d12mshefafa2986f930a8p109e0fjsn2aaad83c7bac';
-  // getWeatherData: any;
 
-  weatherApiBaseUrl = 'http://localhost:9090/weather';
+
+  weatherApiBaseUrl = 'https://weatherapp-backend-production.up.railway.app/weather';
   getWeatherData: any;
-
-  
 
   constructor(private http:HttpClient) { }
 
   getCurrentWeather(cityName : string): Observable<WeatherData>{
-    // return this.http.get<WeatherData>(this.weatherApiBseUrl, {
-    //   headers: new HttpHeaders()
-    //   .set(this.XRapidAPIHostHeaderName, this.XRapidAPIHostHeaderValue)
-    //   .set(this.XRapidAPIKeyHeaderName, this.XRapidAPIKeyHeaderValue),
-    //   params : new HttpParams()
-    //   .set('q', cityName)
-    //   .set('appid', 'da0f9c8d90bde7e619c3ec47766a42f4')
-    //   .set('appid', 'da0f9c8d90bde7e619c3ec47766a42f4')
-    // })
 
     const params = new HttpParams().set('city', cityName);
     return this.http.get<WeatherData>(this.weatherApiBaseUrl, { params });
-    // return this.http.get<WeatherData>(this.weatherApiBseUrl, {
-    //   params : new HttpParams()
-    //   .set('q', cityName)
-    // })
-    
-    
+
   }
 }
